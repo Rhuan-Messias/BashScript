0221db7db9ceab6f510e2fc76d626bf10de96e19
Rhuan-Messias <messias.ifg@gmail.com>
2024-01-20 12:56:52 -0300

    Powershell Portscan

--- PortScan.ps1
+++ PortScan.ps1
@@ -1,7 +1,18 @@
-param($ip,$port)
-
-if (Test-NetConnection $ip -Port $port -WarningAction SilentlyContinue -InformationLevel Quiet) {
-    echo "Port is Open"
-} else {
-    echo "Port is Closed"
+param($ip)
+if(!$ip){
+    echo "==========> MESSIAS - PORTSCAN <=========="
+    echo "Remember to Input an IP address"
+    echo "Exemplo: .\PortScan.ps1 192.169.0.0"
+} else{
+    $topports = 21,22,3306,80,443
+    #$yourports = 1111
+    #$allports = 1..65535
+    try {foreach($port in $topports){
+            if (Test-NetConnection $ip -Port $port -WarningAction SilentlyContinue -InformationLevel Quiet) {
+            echo "Port $port is Open"
+       }} else {
+            echo "Port $port is Closed"
+        }
+    } catch{}   
 }
+

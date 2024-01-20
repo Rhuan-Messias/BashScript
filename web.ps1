671d52396e76bf458b02a00214e73344502f046d
Rhuan-Messias <messias.ifg@gmail.com>
2024-01-20 13:17:17 -0300

    Powershell web analyzer

---
+++ web.ps1
@@ -0,0 +1,11 @@
+﻿$link = Read-Host "Type your website: "
+try{$web = Invoke-WebRequest -uri "$link" -Method Options
+echo "O servidor roda: "
+$web.headers.server
+echo ""
+echo "O servidor aceita os métodos: "
+$web.headers.allow} catch{}
+echo ""
+echo "Links: "
+$web2 = Invoke-WebRequest -uri "$link"
+$web2.links.href | Select-String http://

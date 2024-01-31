e6666c43df1d65f542802d3df7d55540d92f8d69
Rhuan-Messias <messias.ifg@gmail.com>
2024-01-31 14:01:53 -0400

    direct dns 2.0

--- direct_DNS.sh
+++ direct_DNS.sh
@@ -3,5 +3,5 @@
 domain_list="firewall monitoring monitoramento intranet ns1 ns01 mail ns2 webmail rh sistema system admin logs devs documentos server api docs doc"
 
 for item in ${domain_list}; do 
-	echo $item
-done
+	host $item.$1 
+done | grep -v "NXDOMAIN"

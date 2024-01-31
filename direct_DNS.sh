2791dfa33ed40df78c0f7aaf6e940a8cc2f7c721
Rhuan-Messias <messias.ifg@gmail.com>
2024-01-31 13:58:45 -0400

    direct search DNS

---
+++ direct_DNS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+domain_list="firewall monitoring monitoramento intranet ns1 ns01 mail ns2 webmail rh sistema system admin logs devs documentos server api docs doc"
+
+for item in ${domain_list}; do 
+	echo $item
+done

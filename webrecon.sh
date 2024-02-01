5c8613bf7d27b2b8d1b6a4e7948b85fd47404abb
Rhuan-Messias <messias.ifg@gmail.com>
2024-02-01 16:09:01 -0400

    web recon bash

---
+++ webrecon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wordlist="admin desafio dev documentacao ftp infrasecreta intranet mail ns1 ns2 parsingok piloto rh srvkey trello wordpress www app _restrito uploads login home css js prog acesso _docs AcessoRestrito robots.txt senha key index _bkp filesarquivos img upload img cadastro"
+
+echo "looking for Directories"
+
+for word in ${wordlist};do 
+	response=$(curl -s -H "User-agent: disguised" -o /dev/null -w "%{http_code}" $1/$word/) #here it look for directory because of the "/"
+	if [ $response == "200" ];  then
+		echo "directory found: $1/$word/"
+	fi
+done
+
+echo "looking for archives" 

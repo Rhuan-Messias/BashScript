27ba9e098a8c13e479974c11b0afd640af642a18
Rhuan-Messias <messias.ifg@gmail.com>
2024-02-02 10:44:24 -0300

    webrecon att

--- webrecon.sh
+++ webrecon.sh
@@ -2,6 +2,8 @@
 
 wordlist="admin desafio dev documentacao ftp infrasecreta intranet mail ns1 ns2 parsingok piloto rh srvkey trello wordpress www app _restrito uploads login home css js prog acesso _docs AcessoRestrito robots.txt senha key index _bkp filesarquivos img upload img cadastro"
 
+filetypes="php asp aspx do js phps txt doc docx pdf xls xlsx ppt ovpn sql bak old"
+
 echo "looking for Directories"
 
 for word in ${wordlist};do 
@@ -12,9 +14,11 @@ for word in ${wordlist};do
 done
 
 echo "looking for archives" 
-for word in ${wordlist};do 
-	response=$(curl -s -H "User-agent: disguised" -o /dev/null -w "%{http_code}" $1/$word.$2) #here it look for files with the extention $2
-	if [ $response == "200" ];  then
-		echo "file found: $1/$word.$2"
-	fi
+for word in ${wordlist};do
+       for file in ${filetypes};do
+		response=$(curl -s -H "User-agent: disguised" -o /dev/null -w "%{http_code}" $1/$word.$file) #here it look for files with the extention $2
+		if [ $response == "200" ];  then
+		echo "file found: $1/$word.$file"
+		fi
+	done
 done

b28a2d99117ca2632d1de1338fda7f4d60c8997b
Rhuan-Messias <messias.ifg@gmail.com>
2024-02-07 15:32:34 -0400

    ping scan with scapy 1.0

---
+++ ping_scan_scapy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from scapy.all import * 
+import sys
+
+# verbosity configuration for scapy
+conf.verb = 0
+print("Up Hosts")
+for ip in range(1,255): #remember that the range function only gets until max-1
+    ip_testing = "192.168.1." + str(ip)
+    p_ip = IP(dst=ip_testing)
+    packet = p_ip/ICMP()
+    answer,noanswer = sr(packet,timeout=1)
+    if answer:
+        print("Host is UP:", ip_testing)
+    else:
+        pass
+    

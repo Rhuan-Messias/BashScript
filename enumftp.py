5285cc8505ce09e819e78007827fdd5306375ce8
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-12 17:17:49 -0300

    Update enumftp.py

--- enumftp.py
+++ enumftp.py
@@ -9,6 +9,17 @@ tcp.connect((ip, port))
 banner = tcp.recv(1024) #I need to write in python3
 print(banner)
 
+#Passing my user to the ftp server
 tcp.send('USER ftp\r\n')
 user = tcp.recv(1024)
 print(user)
+
+#Passing the password to the ftp server
+tcp.send("pass ftp\r\n")
+pw = tcp.recv(1024)
+print(pw)
+
+tcp.send("HELP\r\n")
+cmd = tcp.recv(2048)
+print(cmd)
+

95735ce0020978582ffc260588ce5685503c4b33
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-12 17:20:16 -0300

    Update enumftp.py

--- enumftp.py
+++ enumftp.py
@@ -6,19 +6,21 @@ port = input('[+] Enter the Port Number: ')
 tcp = socket.socket()
 tcp.connect((ip, port))
 
+print("[+] Connecting with the Server")
 banner = tcp.recv(1024) #I need to write in python3
 print(banner)
 
-#Passing my user to the ftp server
+print("[+] Sending user Name (anonymous/ftp)") # need to put the Anonymous option later
 tcp.send('USER ftp\r\n')
 user = tcp.recv(1024)
 print(user)
 
-#Passing the password to the ftp server
+print("[+] Sending password (anoymous/ftp)") # need to put the anonymous option here too
 tcp.send("pass ftp\r\n")
 pw = tcp.recv(1024)
 print(pw)
 
+print("[+] Testing cmd with help command")
 tcp.send("HELP\r\n")
 cmd = tcp.recv(2048)
 print(cmd)

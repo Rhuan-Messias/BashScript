46cf537b154bb52dfb3696e08c392ee233b675bc
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-13 11:37:42 -0300

    Update smtpenum.py

--- smtpenum.py
+++ smtpenum.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 #rewrite in python3 <-------------
 
-import socket
-
-ip = input("[+] Enter the IP Address: ")
+import socket,sys
+# Exemple -> python3 smtpenum.py 192.168.0.1 root
+#this will connect to the IP in port 25 and VRFY verify if there's a root user
 
 tcp = socket.socket()
-tcp.connect((ip,25))
+tcp.connect((sys.argv[1],25))
 banner = tpc.recv(1024)
 print(banner)
 
-tcp.send("VRFY root \r\n")
+tcp.send("VRFY root" + sys.argv[2] + "\r\n")
 user = tcp.recv(1024)
 print(user)

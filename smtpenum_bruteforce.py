80026506f0c6a6b1d1f42b58607b18bc06105b7e
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-13 11:46:51 -0300

    Update smtpenum_bruteforce.py

--- smtpenum_bruteforce.py
+++ smtpenum_bruteforce.py
@@ -4,3 +4,10 @@ import socket,sys
 
 file = open("list_of_usernames.txt") #put your wordlist for usernames here
 
+for user in file:
+  tcp = socket.socket()
+  tcp.connect((sys.argv[1],25))
+  banner = tcp.recv(1024)
+  tcp.send("VRFY" + user)
+  print(user)
+

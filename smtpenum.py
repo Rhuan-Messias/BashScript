aef871b8f9f68da3418c81a56c3d614cbbd212a8
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-13 11:35:13 -0300

    Update smtpenum.py

--- smtpenum.py
+++ smtpenum.py
@@ -8,5 +8,8 @@ ip = input("[+] Enter the IP Address: ")
 tcp = socket.socket()
 tcp.connect((ip,25))
 banner = tpc.recv(1024)
-
 print(banner)
+
+tcp.send("VRFY root \r\n")
+user = tcp.recv(1024)
+print(user)

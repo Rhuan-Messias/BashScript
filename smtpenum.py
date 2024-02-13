60b6354b222d363cab77b883303f75aee2af379f
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-13 11:40:09 -0300

    Update smtpenum.py

--- smtpenum.py
+++ smtpenum.py
@@ -2,8 +2,12 @@
 #rewrite in python3 <-------------
 
 import socket,sys
-# Exemple -> python3 smtpenum.py 192.168.0.1 root
-#this will connect to the IP in port 25 and VRFY verify if there's a root user
+
+#check if it the arguments was input
+if len(sys.argv) != 3: 
+  print("[+] Example: python3 smtpenum.py 192.168.0.1 root")
+  sys.exit(0)
+  #this will connect to the IP in port 25 and VRFY verify if there's a root user
 
 tcp = socket.socket()
 tcp.connect((sys.argv[1],25))

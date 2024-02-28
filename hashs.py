0f9eec28498580208767e82568eb07b6638ed54f
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-28 11:39:47 -0300

    Update hashs.py

--- hashs.py
+++ hashs.py
@@ -3,3 +3,6 @@
 import hashlib
 text = input("Type the Text to hash")
 hashlib.md5(text).hexdigest()
+hashlib.sha1(text).hexdigest()
+hashlib.sha256(text).hexdigest()
+hashlib.sha512(text).hexdigest()

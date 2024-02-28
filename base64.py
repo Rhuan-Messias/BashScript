cc6702454e26d1f00082a6cf8f5cdb9d44746cc2
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-02-28 11:43:15 -0300

    Update base64.py

--- base64.py
+++ base64.py
@@ -2,4 +2,7 @@
 
 import base64
 text = input("type your text")
-base64.b64encode(text)
+encode_text = base64.b64encode(text)
+print(encode_text)
+original_text = base64.b64decode(encode_text)
+print(original_text)

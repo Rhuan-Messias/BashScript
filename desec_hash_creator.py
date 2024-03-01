b729748860c74cfeca98a8812557fe388ce89011
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-03-01 18:33:43 -0300

    Update desec_hash_creator.py

--- desec_hash_creator.py
+++ desec_hash_creator.py
@@ -1,8 +1,16 @@
 import hashlib as h
 import base64
 
+wordlist =  "/usr/share/john/password.lst"
 given_hash = b"desec"
-step1 = h.md5(given_hash).hexdigest().encode()
-step2 = base64.b64encode(step1)
-step3 = h.sha1(step2).hexdigest()
-print(step3)
+
+with open(wordlist, "r", encoding="utf-8") as john_wordlist:
+    for item in john_wordlist:
+        step1 = h.md5(given_hash).hexdigest().encode()
+        step2 = base64.b64encode(step1)
+        step3 = h.sha1(step2).hexdigest()
+
+        if step3 == given_hash:
+            print(f"{step3} : {item}")
+        else:
+            pass

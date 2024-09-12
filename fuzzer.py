644f0c44e9ae77f63a8240c72db819d7fed14166
root <root@kali>
2024-09-12 16:54:56 -0300

    teste

--- fuzzer.py
+++ fuzzer.py
@@ -1,1 +1,15 @@
-test
+#!/usr/bin/python
+
+lista = ["A"]
+print (type(lista))
+
+while len(lista) <= 3:
+	lista.append("A"*contador)
+	contador = contador + 100
+
+for dados in lista:
+	print "Fuzzing com SEND %s bytes" %len(dados)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("192.168.0.5", 5800))
+	s.recv(1024)
+	s.send("SEND"+dados+"\r\n")
